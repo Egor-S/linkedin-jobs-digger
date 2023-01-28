0f5d02dfc6dac35d0743d5c556945346ad582d17
Egor Sklyarov <egor.sklyarov.ru@gmail.com>
2023-01-28 23:36:24 +0300

    Fix automap relationship warning

--- viewer/viewer/database.py
+++ viewer/viewer/database.py
@@ -1,14 +1,14 @@
 from typing import List
 
-from sqlalchemy import create_engine, ForeignKey, or_
-from sqlalchemy.orm import sessionmaker, Mapped, mapped_column, relationship
+from sqlalchemy import create_engine, or_
+from sqlalchemy.orm import sessionmaker, Mapped, relationship
 from sqlalchemy.ext.hybrid import hybrid_method
-from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.ext import automap
 
 from .config import get_settings
 
 
-Base = automap_base()
+Base = automap.automap_base()
 engine = create_engine(get_settings().db_uri, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(engine)
 
@@ -22,12 +22,23 @@ class JobCard(Base):
 class JobDescription(Base):
     __tablename__ = 'descriptions'
 
-    id: Mapped[str] = mapped_column(ForeignKey('cards.id'), primary_key=True)
-    card: Mapped['JobCard'] = relationship(back_populates='description')
-
     @hybrid_method
     def contains_keywords(self, keywords: List[str]):
         return or_(*(self.text.ilike(f"%{keyword}%") for keyword in keywords))
 
 
-Base.prepare(autoload_with=engine)
+def get_relationship_name(default):
+    def wrapper(base, local_cls, referred_cls, constraint) -> str:
+        name = default(base, local_cls, referred_cls, constraint)
+        return {
+            'jobdescription_collection': 'description',
+            'jobcard': 'card'
+        }.get(name, name)
+    return wrapper
+
+
+Base.prepare(
+    autoload_with=engine,
+    name_for_scalar_relationship=get_relationship_name(automap.name_for_scalar_relationship),
+    name_for_collection_relationship=get_relationship_name(automap.name_for_collection_relationship)
+)
